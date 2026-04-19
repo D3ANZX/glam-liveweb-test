43432b4be41858eb24d61bd56f972a989e82be38
D3ANZX <gasflorentinodean@gmail.com>
2026-04-19 10:42:53 -0300

    Logo update, done SPA Routes

--- client/src/Settings.tsx
+++ client/src/Settings.tsx
@@ -5,7 +5,7 @@ function Settings(){
     const [isToggled, setToggle] = useState(false);
     return(  
         <div className="mt-30 w-screen flex justify-center max-w-screen ">
-            <div className="rounded-2xl w-80 h-100 p-10 shadow-2xl">
+            <div className="rounded-2xl w-80 h-120 p-10 shadow-2xl">
                 <h1 className='font-bold text-2xl'>Settings</h1>
                 
                 <div className="flex w-full">
@@ -30,7 +30,13 @@ function Settings(){
                 <button className='mt-4 bg-red-900 text-white w-40 h-8 rounded-xl'>PRIVACY POLICY</button>
                 <button className='mt-4 bg-red-900 text-white w-40 h-8 rounded-xl'>DELETE ACCOUNT</button>
                 <button className='mt-4 bg-red-900 text-white w-40 h-8 rounded-xl'>ABOUT</button>
-
+                
+                <div className="mt-5">
+                    <p className='text-l font-semibold'>Developers:</p>
+                    <p className='text-sm'>Florentino Dean P. Gas</p>
+                    <p className='text-sm'>Jenmar Oliveros</p>
+                    <p className='text-sm'>BJ B. Saycon</p>
+                </div>
             </div>
         </div>
     );
